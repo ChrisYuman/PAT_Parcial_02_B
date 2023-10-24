14d7aae3e2c906f890cc055bb6faa8bceecd532b
Christopher Yuman <chrisyumanv@gmail.com>
2023-10-24 07:39:21 -0600

    Ejercicio 1 Resuelto

--- PAT_Parcial_01_A/Ejercicio01.cpp
+++ PAT_Parcial_01_A/Ejercicio01.cpp
@@ -1,6 +1,23 @@
 #include "Ejercicio01.h"
 
+
 Node<char>* Ejercicio01::remove(Node<char>* head)
 {
-    return nullptr;
+    Node<char>* current = head;
+
+    while (current != nullptr && current->next != nullptr)
+    {
+        if (current->value == current->next->value)
+        {
+            Node<char>* temp = current->next;
+            current->next = current->next->next;
+            delete temp;
+        }
+        else
+        {
+            current = current->next;
+        }
+    }
+
+    return head;
 }
