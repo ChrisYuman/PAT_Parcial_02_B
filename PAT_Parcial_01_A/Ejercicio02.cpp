2dd4ba80cbc4255a85cf0be2fc8dcf29f6ad02d2
Christopher Yuman <chrisyumanv@gmail.com>
2023-10-24 07:55:37 -0600

    Ejercicio 02, con errores

--- PAT_Parcial_01_A/Ejercicio02.cpp
+++ PAT_Parcial_01_A/Ejercicio02.cpp
@@ -1,6 +1,32 @@
 #include "Ejercicio02.h"
+#include <vector>
+#include <algorithm>
 
 int Ejercicio02::countStudents(vector<char>* students, vector<char>* sandwiches)
 {
-    return 0;
+    int n = students->size();
+
+    std::vector<int> studentCount(128, 0);  
+    std::vector<int> sandwichCount(128, 0); 
+
+    for (int i = 0; i < n; i++) {
+        studentCount[(*students)[i]]++;
+        sandwichCount[(*sandwiches)[i]]++;
+    }
+
+    int studentsLeft = n;
+
+    for (int i = 0; i < n; i++) {
+        char studentPreference = (*students)[i];
+        if (studentCount[studentPreference] > 0 && sandwichCount[studentPreference] > 0) {
+            studentCount[studentPreference]--;
+            sandwichCount[studentPreference]--;
+            studentsLeft--;
+        }
+        else {
+            break;
+        }
+    }
+
+    return studentsLeft;
 }
