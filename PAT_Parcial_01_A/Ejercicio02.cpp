62886fbfa7f6133e7bcb6fa4173340e6a20731e9
Christopher Yuman <chrisyumanv@gmail.com>
2023-10-24 08:34:22 -0600

    Ejercicio 02, resuelto

--- PAT_Parcial_01_A/Ejercicio02.cpp
+++ PAT_Parcial_01_A/Ejercicio02.cpp
@@ -1,32 +1,26 @@
 #include "Ejercicio02.h"
 #include <vector>
-#include <algorithm>
 
-int Ejercicio02::countStudents(vector<char>* students, vector<char>* sandwiches)
+int Ejercicio02::countStudents(std::vector<char>* students, std::vector<char>* sandwiches)
 {
-    int n = students->size();
+    
 
-    std::vector<int> studentCount(128, 0);  
-    std::vector<int> sandwichCount(128, 0); 
-
-    for (int i = 0; i < n; i++) {
-        studentCount[(*students)[i]]++;
-        sandwichCount[(*sandwiches)[i]]++;
-    }
-
-    int studentsLeft = n;
-
-    for (int i = 0; i < n; i++) {
-        char studentPreference = (*students)[i];
-        if (studentCount[studentPreference] > 0 && sandwichCount[studentPreference] > 0) {
-            studentCount[studentPreference]--;
-            sandwichCount[studentPreference]--;
-            studentsLeft--;
+    int studentI = 0;
+    int sandwichI = 0;
+    while (studentI < students->size() && sandwichI < sandwiches->size()) {
+        if (students->at(studentI) == sandwiches->at(sandwichI)) {
+            students->erase(students->begin() + studentI);
+            sandwiches->erase(sandwiches->begin() + sandwichI);
+            studentI = 0;
+            sandwichI = 0;
         }
         else {
-            break;
+            studentI++;
         }
     }
+    return students->size();
 
-    return studentsLeft;
+    if (students->empty() || sandwiches->empty()) {
+        return students->size();
+    }
 }
